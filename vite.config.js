11791b021fd019a2d40aa7edf33ec40d2b59e3eb
BafskITClub <khalid.bafskitclub@gmail.com>
2026-05-25 18:43:39 +0600

    commited

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // আপনার প্রজেক্টে এই লাইনটি থাকতে পারে
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  base: '/bafskitclub.github.io/', // bafskitclub.github.io 
+})  
