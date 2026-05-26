1c822114bb42472323aa15384944855b0f857e9f
BafskITClub <khalid.bafskitclub@gmail.com>
2026-05-26 17:52:57 +0600

    Clean project build and setup gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // এই প্লাগইনটি Tailwind v4 এরর ফিক্স করবে
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
-  build: {
-    // ভিট-এর ডিফল্ট প্রোডাকশন মিনফায়ার ব্যবহার করা হবে যা কোনো এক্সটার্নাল ডিপেন্ডেন্সি খুঁজবে না
-    cssMinify: true
-  }
+  plugins: [
+    react(),
+    tailwindcss() // প্লাগইনটি এখানে অ্যাক্টিভেট করা হলো
+  ],
 })
