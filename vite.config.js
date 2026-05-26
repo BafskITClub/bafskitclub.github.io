51b04226ef6bd013aeb83ffcaa5453cede28679f
BafskITClub <khalid.bafskitclub@gmail.com>
2026-05-26 17:48:55 +0600

    Fix build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // আপনার প্রজেক্টে এই লাইনটি থাকতে পারে
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  css: {
-    // lightningcss-এর এরর বন্ধ করার জন্য এটি esbuild-এ সেট করা হলো
-    transformer: 'postcss',
-    minify: 'esbuild' 
-  },
   build: {
-    cssMinify: 'esbuild' // CSS মিনফাই করার জন্য esbuild ব্যবহার করবে
+    // ভিট-এর ডিফল্ট প্রোডাকশন মিনফায়ার ব্যবহার করা হবে যা কোনো এক্সটার্নাল ডিপেন্ডেন্সি খুঁজবে না
+    cssMinify: true
   }
 })
