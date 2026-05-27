2f1ec19837f866500e63cca7c4aa315fd144e370
Ekram Ezaz <khalid.bafskitclub@gmail.com>
2026-05-27 19:06:06 +0600

    fixing deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // এই প্লাগইনটি Tailwind v4 এরর ফিক্স করবে
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [
     react(),
-    tailwindcss() // প্লাগইনটি এখানে অ্যাক্টিভেট করা হলো
+    tailwindcss(),
   ],
 })
